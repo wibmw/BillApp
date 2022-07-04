551901233131b254ee7f69956111b03db2826a48
wibmw <ilaouer@outlook.com>
2022-07-04 11:48:03 +0200

    newBill coverage

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -76,6 +76,7 @@ export default class NewBill {
   }
 
   // not need to cover this function by tests
+  /* istanbul ignore next */
   updateBill = (bill) => {
     if (this.store) {
       this.store
